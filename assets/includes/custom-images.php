6784229e51144b80194c3855839d393d81fd1f69
anuj <anujpandeybbdu@gmail.com>
2020-05-17 22:25:44 +0530

    update view

--- assets/includes/custom-images.php
+++
@@ -1,7 +0,0 @@
-	<img src="assets/base/img/content/fces/img-sq-ph.jpg" alt="" class="own-design-img1and2" />
-	<img src="assets/base/img/content/fces/img-sq-ph.jpg" alt="" class="own-design-img1and2" />
-	<img src="assets/base/img/content/fces/img-sq-ph.jpg" alt="" class="own-design-img3" />
-
-	<img src="assets/base/img/content/fces/img-sq-ph.jpg" alt="" class="own-design-img1and2 own-design-last-row" />
-	<img src="assets/base/img/content/fces/img-sq-ph.jpg" alt="" class="own-design-img1and2 own-design-last-row" />
-	<img src="assets/base/img/content/fces/img-sq-ph.jpg" alt="" class="own-design-img3 own-design-last-row" />
