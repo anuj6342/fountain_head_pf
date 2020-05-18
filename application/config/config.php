e30c5ea75638f87ed8a4ea20a417e31142bcad26
anuj pandey <anujpandeybbdu@gmail.com>
2020-05-18 23:23:50 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/my_project/portfolio/fountain_head_pf/';
+$config['base_url'] = 'http://localhost/portflio/fountain_head_pf/';
 
 /*
 |--------------------------------------------------------------------------
